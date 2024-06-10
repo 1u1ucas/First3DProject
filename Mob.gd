73257825d13017b7e9953efebee935e4b1092c26
1u1ucas <lucasleveque17@gmail.com>
2024-06-10 16:49:43 +0200

    add the spawn location of the mob and the spawn script

--- Mob.gd
+++ Mob.gd
@@ -7,16 +7,21 @@ export var max_speed = 18
 
 var velocity = Vector3.ZERO
 
+func _ready():
+	# Ensure the signal is connected
+	if not $VisibilityNotifier.is_connected("screen_exited", self, "_on_VisibilityNotifier_screen_exited"):
+		$VisibilityNotifier.connect("screen_exited", self, "_on_VisibilityNotifier_screen_exited")
+
 func _physics_process(_delta):
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
 
 # This function will be called from the Main scene.
-func initialize(start_position, player_position):
+func initialize(start_position, player_translation):
 	# We position the mob by placing it at start_position
 	global_transform.origin = start_position
 	
-	# and rotate it towards player_position, so it looks at the player.
-	look_at(player_position, Vector3.UP)
+	# and rotate it towards player_translation, so it looks at the player.
+	look_at(player_translation, Vector3.UP)
 	
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
@@ -31,6 +36,6 @@ func initialize(start_position, player_position):
 	# We then rotate the velocity vector based on the mob's Y rotation
 	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
-	
+
 func _on_VisibilityNotifier_screen_exited():
 	queue_free()
